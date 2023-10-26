75fb3c5c9a5f378c63e6a62d0633e2a13b0ff002
honza <hhohha@seznam.cz>
2023-10-26 19:02:17 +0200

    .

--- layout.py
+++ layout.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from PySimpleGUI import RELIEF_RAISED, RELIEF_RIDGE, VerticalSeparator, HorizontalSeparator
 
 import dbif
 from enums import ClsType
@@ -12,7 +13,23 @@ def get_main_window_layout():
     ##### main table frame ####################################################
     transactionsTable = sg.Table(values = [[]], key='tbl_transactions', headings=['id', 'date', 'amount', 'message', 'type', 'category', 'status'],
                                  auto_size_columns=False, num_rows=65, col_widths=[5, 12, 10, 100, 20, 20, 10], enable_events = True)
-    frameTransactionTable = sg.Frame(layout=[[transactionsTable]], title='Transactions')
+
+    frameButtons = sg.Frame(layout=[
+        [sg.Button('Save one', key='btn_save_one', p=((0, 20), (0, 0))),
+         sg.Button('Change type', key='btn_change_type', p=((0, 20), (0, 0))),
+         sg.Button('Change category', key='btn_change_category', p=((0, 20), (0, 0))),
+         sg.Button('Hide line', key='btn_hide_line', p=((0, 20), (0, 0))),
+         sg.Button('Remove line from DB', key='btn_remove_line', p=((0, 20), (0, 0))),
+         sg.Button('Load from file', key='btn_load_from_file', p=((0, 130), (0, 0))),
+
+         sg.Text('Total credit:'), sg.Text('', key='txt_total_credit', p=((0, 25), (0, 0))),
+         sg.Text('Total debit:'), sg.Text('', key='txt_total_debit', p=((0, 25), (0, 0))),
+         sg.Text('Total amount:'), sg.Text('', key='txt_total_amount', p=((0, 25), (0, 0))),
+         sg.Text('Transaction count:'), sg.Text('', key='txt_total_cnt')
+        ]
+    ], title='',expand_x=True)
+
+    frameTransactionTable = sg.Frame(layout=[[transactionsTable], [frameButtons]], title='Transactions')
 
     ##### details frame #######################################################
     frameTransactionDetails = sg.Frame(layout=[
@@ -46,63 +63,57 @@ def get_main_window_layout():
         [sg.Text('', key='txt_detail_type')]
     ], title='Type')
 
+    sep = sg.HorizontalSeparator()
+
     frameSignatures = sg.Frame(layout=[
         [sg.Table(values=[[]], key='tbl_signatures', headings=['signature'], auto_size_columns=False, num_rows=10, col_widths=[25])],
         [sg.Button('Add signature', key='btn_add_sign'), sg.Button('Remove signature', key='btn_remove_sign')]
     ], title='Signatures')
 
-    frameDetails = sg.Frame(layout=[[frameTransactionDetails], [frameTags], [frameCategory], [frameTrType], [frameSignatures]], title='Details')
-
-    ##### buttons frame #######################################################
-    frameButtons = sg.Frame(layout=[
-        [sg.Button('Save all', key='btn_save_all'), sg.Button('Save one', key='btn_save_one')],
-        [sg.Button('Change type', key='btn_change_type'), sg.Button('Change category', key='btn_change_category')],
-        [sg.Button('Load data', key='btn_load_data'), sg.Button('Load from file', key='btn_load_from_file')],
-        [sg.Button('Hide line', key='btn_hide_line'), sg.Button('Remove line from DB', key='btn_remove_line')],
-        [sg.Button('Backup DB', key='btn_backup'), sg.Button('Restore DB', key='btn_restore')]
-    ], title='Buttons')
+    frameDetails = sg.Frame(layout=[[frameTransactionDetails], [frameTags], [frameCategory], [frameTrType], [sep], [frameSignatures]], title='', pad=10, border_width=5, expand_x=True, expand_y=True)
 
     ##### filter frame ########################################################
     frameFilter = sg.Frame(layout=[
         [
-            sg.Text('Date from:'), sg.Input(key='filter_date_from'),
-            sg.Text('Date to:'), sg.Input(key='filter_date_to'),
-            sg.Text('Amount min:'), sg.Input(key='filter_amount_min'),
-            sg.Text('Amount max:'), sg.Input(key='filter_amount_max'),
-            sg.Text('Description:'), sg.Input(key='filter_desc')
+            sg.Text('Date from:', p=((0, 0), (10, 0))), sg.Input(key='filter_date_from', p=((0, 0), (10, 0))),
+            sg.Text('Date to:', p=((0, 0), (10, 0))), sg.Input(key='filter_date_to', p=((0, 0), (10, 0))),
+            sg.Text('Amount min:', p=((0, 0), (10, 0))), sg.Input(key='filter_amount_min', p=((0, 0), (10, 0))),
+            sg.Text('Amount max:', p=((0, 0), (10, 0))), sg.Input(key='filter_amount_max', p=((0, 0), (10, 0))),
+            sg.Text('Description:', p=((0, 0), (10, 0))), sg.Input(key='filter_desc', p=((0, 0), (10, 0)))
         ],
+        [HorizontalSeparator(p=((0, 0), (10, 10)))],
         [
             sg.Radio('Only credit', 'filter_credit_debit', key='radio_filter_cred'),
             sg.Radio('Only debit', 'filter_credit_debit', key='radio_filter_deb'),
             sg.Radio('Both', 'filter_credit_debit', key='radio_filter_both', default=True)
         ],
-        [
-            sg.Button('<', key='btn_filter_prev_month'),
-            sg.Button('This month', key='btn_filter_this_month'),
-            sg.Button('>', key='btn_filter_next_month')
-        ],
+        [HorizontalSeparator(p=((0, 0), (10, 10)))],
         [
             sg.Text('Type:'), sg.Listbox(values=tr_types, key='filter_type', size=(25,10), select_mode=sg.LISTBOX_SELECT_MODE_EXTENDED),
             sg.Text('Category:'), sg.Listbox(values=categories, key='filter_category', size=(25,10), select_mode=sg.LISTBOX_SELECT_MODE_EXTENDED),
             sg.Text('Tag:'), sg.Listbox(values=tags, key='filter_tags', size=(25,10), select_mode=sg.LISTBOX_SELECT_MODE_EXTENDED)
         ],
-        [sg.Button('Clear filters', key='btn_clear_filters')]
-    ], title='Filters')
+        [HorizontalSeparator(p=((0, 0), (10, 10)))],
+        [
+            sg.Button('Load data', key='btn_load_data', p= ((10, 50), (10, 10))),
+            sg.Button('<', key='btn_filter_prev_month'),
+            sg.Button('This month', key='btn_filter_this_month'),
+            sg.Button('>', key='btn_filter_next_month', p= ((10, 50), (10, 10))),
+            sg.Button('Clear filters', key='btn_clear_filters'),
+        ]
+    ], title='Filters', p=((0, 0), (20, 0)))
 
-    ##### summary frame #######################################################
-    frameSummary = sg.Frame(layout=[
-        [sg.Text('Total credit:'), sg.Text('', key='txt_total_credit')],
-        [sg.Text('Total debit:'), sg.Text('', key='txt_total_debit')],
-        [sg.Text('Total amount:'), sg.Text('', key='txt_total_amount')],
-        [sg.Text('Transaction count:'), sg.Text('', key='txt_total_cnt')],
-        [sg.Text('Last backup:'), sg.Text('', key='txt_last_backup')]
-    ], title='Summary')
+    ##### backup frame #######################################################
+    frameBkp = sg.Frame(layout=[
+        [sg.Button('Backup DB', key='btn_backup'), sg.Button('Restore DB', key='btn_restore')],
+        [sg.Text('Last backup:'), sg.Text('', key='txt_last_backup', p = ((0, 0), (5, 0)))]
+    ], title='Backup', p=((100, 0), (290, 0)))
 
     return [
         [frameTransactionTable, frameDetails],
-        [frameButtons, frameFilter, frameSummary]
+        [frameFilter, frameBkp]
     ]
 
 layout = get_main_window_layout()
 window = sg.Window('Welcome to accounting', layout, default_element_size=(12, 1), element_padding=(1, 1), return_keyboard_events=True,
-                       resizable=False, finalize=True).finalize()
+                   resizable=False, finalize=True).finalize()
